ab211b8f4fe6d4f32e30e72922d88cf35ab0012f
ChaseWNorton <chasenorton@me.com>
2018-02-23 10:59:24 -0800

    First Commit.  Webpack is now configured and a simple React hello world is displaying

---
+++ client/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Homepage from './components/homepage.jsx'
+ReactDOM.render(
+  <Homepage />,
+  document.getElementById('app')
+);
---
+++ server/server.js
@@ -0,0 +1,8 @@
+const serve = require('koa-static-server');
+const koa = require('koa');
+const app = new koa();
+
+app.use(serve({rootDir: '../dist'}));
+
+app.listen(3000);
+console.log('Listening on port 3000');
