ee8a924be1bed64868db65a735d41285880d4f59
ChaseWNorton <chasenorton@me.com>
2018-02-25 22:41:06 -0800

    Worked with drawing angles

--- client/components/homepage.jsx
+++ client/components/homepage.jsx
@@ -1,41 +1,88 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text, Line, TextPath } from 'react-konva';
+const Konva = require('konva');
+
 
 class Homepage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      canvasEle: '',
-      canvasColor: 'red',
+      color: 'green',
+      blog: 'Hellow World, this is my blog post about crazy cats!',
+      date: 'February 22',
+      title: 'Week One Up'
     };
-    this.updateCanvas = this.updateCanvas.bind(this);
-    this.onClick = this.onClick.bind(this);
-  }
-
-  componentDidMount() {
-    this.updateCanvas(this.canvasEle);
+    this.onClick = this.onClick.bind(this)
   }
 
   onClick() {
-    this.setState({
-      canvasColor: 'blue',
-    }, () => {this.updateCanvas(this.canvasEle)});
-  }
-
-  updateCanvas(node) {
-    const cx = node.getContext('2d');
-    cx.beginPath();
-    cx.moveTo(10, 90);
-    cx.quadraticCurveTo(0, 0, 90, 90);
-    cx.lineTo(60, 10);
-    cx.closePath();
-    cx.stroke();
+    alert('Going to the new page!')
   }
 
   render() {
     return(
       <div>
-        <canvas ref={canvas => this.canvasEle = canvas} onClick={this.onClick} height="10000"></canvas>
-      </div>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+            <Text
+              x={window.innerWidth/2 - 20}
+              y={20}
+              width={200}
+              text={this.state.date}
+              onClick={this.onClick}
+            />
+          <Text
+            x={window.innerWidth/2 - 20}
+            y={50}
+            width={100}
+            text={this.state.title}
+            onClick={this.onClick}
+          />
+          <Text
+            x={window.innerWidth/2 - 20}
+            y={75}
+            width={100}
+            lineHeight={1}
+            text={this.state.blog}
+            onClick={this.onClick}
+          />
+          <Line
+            points={[window.innerWidth/2 + 110, 25, window.innerWidth/2 + 90, 25]}
+            stroke={'black'}
+            strokeWidth={0.6}
+
+            />
+          <Line
+            points={[window.innerWidth/2 + 110, 25, window.innerWidth/2 + 110, 200]}
+            stroke={'black'}
+            strokeWidth={0.6}
+            />
+          <Text
+            x={window.innerWidth/2 - 20}
+            y={20}
+            width={100}
+            text={this.state.date}
+            onClick={this.onClick}
+          />
+          <Text
+            x={window.innerWidth/2 - 20}
+            y={50}
+            width={100}
+            text={this.state.title}
+            onClick={this.onClick}
+          />
+          <Text
+            x={window.innerWidth/2 - 20}
+            y={75}
+            width={100}
+            lineHeight={1}
+            text={this.state.blog}
+            onClick={this.onClick}
+          />
+        </Layer>
+      </Stage>
+    </div>
 
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,10 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
+
 
 module.exports = {
   entry: './client/app.jsx',
   output: {
-    path: path.resolve('dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js'
   },
 
@@ -19,5 +14,4 @@ module.exports = {
       {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
 };
