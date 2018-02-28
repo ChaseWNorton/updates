dbd6099a38edef610e21be0b0e9c85e080cd2d32
ChaseWNorton <chasenorton@me.com>
2018-02-27 23:39:41 -0800

    Figured out the timeline display

--- client/components/updates.jsx
+++ client/components/updates.jsx
@@ -9,20 +9,34 @@ class Homepage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      color: 'green',
-      blog: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-      date: 'February 22',
-      title: 'Week One Up',
+      posts: [
+        {id: 1,
+        article: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        summary: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        date: 'February 22, 2018',
+        title: 'Week One Up',},
+        {id: 2,
+          article: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+          summary: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+          date: 'February 22, 2018',
+          title: 'Week One Up',},
+      ],
       foundedDate:  'January 15th 2018',
     };
   }
 
+
+
   render() {
     return(
       <div className="update-container">
-          <TimelineLeft title={this.state.title} date={this.state.date} blog={this.state.blog} />
-          <TimelineRight title={this.state.title} date={this.state.date} blog={this.state.blog} />
-          <TimelineLeft title={this.state.title} date={this.state.date} blog={this.state.blog} />
+        {this.state.posts.map(ele => {
+            if (ele.id % 2 !== 0) {
+              return <TimelineLeft key={ele.id} title={ele.title} date={ele.date} blog={ele.summary} />;
+            } else {
+              return <TimelineRight key={ele.id} title={ele.title} date={ele.date} blog={ele.summary} />;
+            }
+        })}
           <Founded foundedDate={this.state.foundedDate} />
       </div>
     )
