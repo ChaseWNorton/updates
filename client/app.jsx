5cb153cb5ba973afa786d8c9b5d13b403d130cb7
ChaseWNorton <chasenorton@me.com>
2018-05-08 11:26:58 -0700

    Broke up design

--- client/app.jsx
+++ client/app.jsx
@@ -5,3 +5,5 @@ import Updates from './components/updates.jsx'
 ReactDOM.render(
   <Updates ID={window.location.pathname.slice(1, window.location.pathname.length - 1)} />, document.getElementById('updates')
 );
+
+//Renders solo now
